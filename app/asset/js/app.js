04273eb1c10e36bda2799dd5aa013b9af6939374
geehaejeong <brainstation@BrainStations-MacBook-Air-8.local>
2016-03-03 22:01:28 -0500

    updated landing page

---
+++ app/asset/js/app.js
@@ -0,0 +1,15 @@
+//this stuff happens when HTML is ready
+
+// $(document).ready(function(){
+// $('.lead-form').css('display','none');
+// $('.info').css('display','block');
+
+// 	$('.submit').click(function(event){
+// 		event.preventDefault();
+// 		$('.lead-form').animate({width:'toggle'},350);
+// 		$('.info').css('display','none');
+// 	})
+
+
+
+// });
